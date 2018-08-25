dab76f60cfad1575720df953626e8216c44e82ef
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2018-08-25 17:57:41 +0900

    Add current_difference, worked_hours, worked_days methods

--- jobcan_checker_script.rb
+++ jobcan_checker_script.rb
@@ -1,8 +1,9 @@
 require 'capybara'
 require 'capybara/dsl'
 require 'selenium-webdriver'
+require 'pry'
 
-Capybara.default_driver = :selenium_chrome
+Capybara.default_driver = :selenium_chrome_headless
 Capybara.app_host = ENV['BASE_URL']
 
 module Crawler
@@ -18,13 +19,43 @@ module Crawler
 
     def visit_attendance_page
       click_on '勤怠'
-      sleep 5
       switch_to_window { title == "JOBCAN MyPage: #{ENV['MY_NAME']}" }
       click_on '出勤簿'
     end
+
+    def current_difference
+       worked_hours - (worked_days * 8).to_f
+    end
+
+    private
+
+    # return [Float]
+    def worked_hours
+      worked_time = first(:xpath, "//th[contains(text(),'実労働時間')]/following-sibling::*").text
+      hours, minutes = worked_time.split(':')
+      hours.to_f + (minutes.to_f / 60)
+    end
+
+    # return [Integer]
+    def worked_days
+      worked_days = first(:xpath, "//th[contains(text(),'実働日数')]/following-sibling::*").text
+      worked_days.to_i
+    end
+  end
+end
+
+class Float
+  def to_time
+    "#{self.to_i} 時間 #{((self - self.to_i) * 60).round} 分"
   end
 end
 
 crawler = Crawler::Jobcan.new
 crawler.login
 crawler.visit_attendance_page
+
+if ARGV.first == 'current_difference'
+  puts crawler.current_difference.to_time
+else
+  puts '今日も頑張ろうな'
+end
